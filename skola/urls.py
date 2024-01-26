291c07d062a5d2d7526e8d2628a9aa3799efe996
ryoNtahC <ryontahc@gmail.com>
2024-01-26 11:33:27 +0100

    skola prepojenia

--- skola/urls.py
+++ skola/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.vypis_skola, name='vypis-skola'),
-    path('triedy/', views.vypis_triedy, name='vypis-triedy'),
-    path('ucitelia/', views.vypis_ucitela, name='vypis-ucitela'),
-    path('studenti/', views.vypis_studenta, name='vypis-studenta'),
+    path('', views.vypis_skola, name='skola'),
+    path('triedy/', views.vypis_triedy, name='triedy'),
+    path('ucitelia/', views.vypis_ucitela, name='ucitelia'),
+    path('studenti/', views.vypis_studenta, name='studenti'),
+    path("triedy/<trieda>/", views.vypis_trieda, name='trieda'),
 ]
--- skola/views.py
+++ skola/views.py
@@ -18,3 +18,14 @@ def vypis_triedy(request):
 def vypis_ucitela(request):
     ucitelia = Ucitel.objects.all().order_by("priezvisko")
     return render(request, "skola/index.html", {"ucitelia":ucitelia})
+
+def vypis_trieda(request, trieda):
+    trieda_obj = Trieda.objects.get(nazov=trieda)
+    studenti = Student.objects.filter(trieda_id=trieda_obj.pk).order_by("priezvisko")
+    studenti_list = []
+    for student in studenti:
+        studenti_list.append(f"{student.meno} {student.priezvisko}")
+    ucitel = Ucitel.objects.get(trieda_id=trieda_obj.pk)
+    ucitel = f"{ucitel.titul} {ucitel.meno} {ucitel.priezvisko}"
+    #return HttpResponse(f'{trieda}<br>{ucitel}<br>{studenti_list}')
+    return render(request, "skola/trieda_list.html", {"trieda":trieda, "ucitel":ucitel, "studenti":studenti_list})
